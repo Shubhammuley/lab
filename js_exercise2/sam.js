5da9c11a79d1a6093e74d4e11b98bac7bb8e7f16
shubhammuley <shubham.muley@rapidops.com>
2020-02-20 15:29:11 +0530

    update

--- js_exercise2/sam.js
+++ js_exercise2/sam.js
@@ -1,39 +1,12 @@
-function display(array){
-  let reading = [];
-   travelling=[],music=[],drawing=[];
-
-  for ( let i=0 ; i < array.length ; i++){
-    if(array[i].hobbies.indexOf("reading") > -1)  {
-      reading.push(array[i].name)
-
-    }
-    if(array[i].hobbies.indexOf("travelling") > -1)  {
-      travelling.push(array[i].name)
-
-    }
-    if(array[i].hobbies.indexOf("drawing") > -1)  {
-      drawing.push(array[i].name)
-    }
-    if(array[i].hobbies.indexOf("music") > -1)  {
-      music.push(array[i].name)
-    }
-
-
-  }
-
-
-  console.log("Reading : " + reading);
-  console.log("travelling : "+ travelling);
-  console.log("drawing  : "+  drawing);
-  console.log("music : "+ music);
+let a = {
+  name:"dsd",
+  id:55
 }
+Object.defineProperty(a,"name",{writable: false , enumerable: false})
+console.log(Object.getOwnPropertyDescriptor(a,'name'));
 
-let array = [
-    { id:1, name:"abc" , hobbies:["reading", "travelling", "music" ]},
-    { id:2, name:"xyz" , hobbies:[ "reading", "drawing", "music" ]},
-    { id:2, name:"dd" , hobbies:[ "drawing", "music" ]},
-    { id:2, name:"ee" , hobbies:[ "reading",]},
-    { id:2, name:"ff" , hobbies:[ "travelling", "reading", "music" ]}
-  ];
+//a.name ="dsaddsd"
+//a.id=55445
+console.log(a);
 
-  display(array)
+for (let key in a) console.log(key);
