15ce5486f734429efd308d98477481786698c709
unknown <shubham.muley@rapiddops.com>
2020-02-29 14:35:40 +0530

    update

--- js_exercise3/task1/js/admin.js
+++ js_exercise3/task1/js/admin.js
@@ -310,3 +310,32 @@ function logout(){
   
   alert("logged out sucessfully")
 }
+
+//----------------------------------addCourse---------------------------
+
+function addCourse()
+{
+    let course_name= document.getElementById("course_name").value ; 
+    let course_link= document.getElementById("course_link").value;
+    
+    let file = document.getElementById("course_file").value
+     file=file.slice(12);
+     file="/img/"+file;
+    
+     let courses={coursename:course_name , courselink:course_link , coursefile:file}
+    let course_data;
+    if(localStorage.getItem("course") === null){
+        localStorage.course =  JSON.stringify([courses])
+  
+    }
+    else{
+      course_data=JSON.parse(localStorage.course);
+      course_data.push(courses)
+        localStorage.course = JSON.stringify(course_data)
+
+        window.location.replace("/admin.html")
+  
+    }
+
+
+}
